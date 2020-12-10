27c43bac47fce8e35316391850f7deda4511a7ed
Juan Pablo <juan.eslava@ergosign.de>
2020-12-10 18:27:34 +0100

    Comp: started Home Comp

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-
+// material imports
+import {  AppBar,  } from '@material-ui/core';
+import { TabContext, TabList, Tab, TabPanel } from '@material-ui/lab';
 class Home extends Component {
 
     render() {
         
         return (
             <div>
-                Home page          
+                <h2>Home</h2>         
+                <div className='list-container'>
+                    <h3>Ongoing Polls</h3>
+                    <TabContext>
+                        <AppBar position="static">
+                            <TabList>
+                                <Tab label="Answered Questions" value="1" />
+                                <Tab label="Unanswered Questions" value="2" />    
+                            </TabList>
+                        </AppBar>
+                        <TabPanel value="1">Answered Questions Panel</TabPanel>
+                        <TabPanel value="2">Unanswered Questions Panel</TabPanel>
+                    </TabContext>
+                </div>
             </div>
         )
     }
